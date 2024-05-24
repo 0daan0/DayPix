494d982be079b3218dd1902208afe3d6e67e2ea7
0daan0 <daniel_guurink@hotmail.com>
2024-05-24 12:44:11 +0200

    Code cleanup

--- DayPix/DayPix.ino
+++ DayPix/DayPix.ino
@@ -1,7 +1,6 @@
 #include <WiFi.h>
 #include <ESPAsyncWebSrv.h>
 #include <EEPROM.h>
-
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include "rgb_effects.h"
@@ -18,23 +17,13 @@
 #include <ArtnetETH.h>
 ArtnetReceiver artnet;
 #else
+
 #include <ArtnetWiFi.h>
 ArtnetWiFiReceiver artnet;
 #endif
 
 ledDriver led;
 RGBEffects effect;
-
-// DMX array data
-const uint8_t* bdata;
-uint16_t bsize;
-
-const uint8_t* bdata2;
-uint16_t bsize2;
-
-int cb1calls=0;
-int cb2calls=0;
-// Artnet Callback
 
 void callback(const uint8_t* data, const uint16_t size) {    
   if (recvUniverse.indexOf(String(artnet.universe())) == -1) {
@@ -46,54 +35,23 @@ void callback(const uint8_t* data, const uint16_t size) {
 
 void callback1(const uint8_t* data, const uint16_t size) {
  if (recvUniverse.indexOf(String(artnet.universe())) == -1) {
-      // The recvUniverse does not contain artnet.universe(), so add it
       recvUniverse += "U" + String(artnet.universe()) + "U";
   }
   led.writePixelBuffer(data, size, NrOfLeds, DmxAddr, 0);
 }
 
 void callback2(const uint8_t* data, const uint16_t size) {
-  //bdata2 = data;
-  //bsize2 = size;
-  //fillArray(data, size);
   if (recvUniverse.indexOf(String(artnet.universe())) == -1) {
-      // The recvUniverse does not contain artnet.universe(), so add it
       recvUniverse += "U" + String(artnet.universe()) + "U";
   }
   led.writePixelBufferPort2(data, size, NrOfLeds, DmxAddr, 0);
-  /*
-   if (!b_reverseArray == 1) {
-      uint8_t reversedData[size];
-      memcpy(reversedData, data, size); // Make a copy of original data
-      reverseArray(reversedData, size); // Reverse the copied array
 
-      led.writePixelBufferPort2(reversedData, size, NrOfLeds, DmxAddr, 0);
-  } else {
-      led.writePixelBufferPort2(data, size, NrOfLeds, DmxAddr, 0);
-  }
- */
-  //led.showBufferP2();
 }
 void callback3(const uint8_t* data, const uint16_t size) {
-  //bdata2 = data;
-  //bsize2 = size;
-  //fillArray(data, size);
   if (recvUniverse.indexOf(String(artnet.universe())) == -1) {
-      // The recvUniverse does not contain artnet.universe(), so add it
       recvUniverse += "U" + String(artnet.universe()) + "U";
   }
   led.writePixelBufferPort2(data, size, NrOfLeds, DmxAddr, 0);
-  /*
-  if (!b_reverseArray == 1) {
-      uint8_t reversedData[size];
-      memcpy(reversedData, data, size); // Make a copy of original data
-      reverseArray(reversedData, size); // Reverse the copied array
-
-      led.writePixelBufferPort2(reversedData, size, NrOfLeds, DmxAddr, 0);
-  } else {
-      led.writePixelBufferPort2(data, size, NrOfLeds, DmxAddr, 0);
-  }
-  */
 }
 
 // Task to get Artnet data frame and write to pixel buffer
@@ -129,6 +87,7 @@ void artnetTask2(void* parameter) {
   }
 }
 
+
 #ifdef ETH_CAP
 void conGuardTask(void* parameter) {
   while(1){
@@ -144,20 +103,12 @@ void conGuardTask(void* parameter) {
     }
     while(WiFi.status() == WL_CONNECTED){
       if (led.ethCap){
-               //Serial.println("EthCAP check");
         if (ETH.linkUp()){
           ESP.restart();
         }
       }
       vTaskDelay(5 / portTICK_PERIOD_MS);  // Adjust delay as needed
-    }
-   
-    /*if (ETH.linkUp() && WiFi.isConnected()) {
-      Serial.println("Eth and WIFI Connected");
-      delay(500);
-      ESP.restart();
-    }*/
-    
+    }  
   }
 }
 #endif
@@ -173,34 +124,38 @@ void otaTask(void* parameter) {
 void setup() {
   // init serial for debugging
   Serial.begin(115200);
+  
+  // WHY THIS DELAY?
   delay(1000);
+
   Serial.println("Starting setup...");
   // init eeprom
   EEPROM.begin(512);
   initializeEEPROM();
 
-  
+  // init ethernet of device is capable
   #ifdef ETH_CAP
   ETH.begin();
   #endif
+// #region Retrieve stored configuration
   
-  // debug listing all files on filesystem
-  //listFiles(SPIFFS, "/");
-
-  // Retrieve stored configuration
-  String storedSSID = getStoredString(SSID_EEPROM_ADDR);
-  String storedPassword = getStoredString(PASS_EEPROM_ADDR);
+  // ArtNet settings
   String storedUniverse = getStoredString(UNIVERSE_EEPROM_ADDR);
+  int storedUniverseStart = getStoredString(UNIVERSE_START_EEPROM_ADDR).toInt();
+  int storedUniverseEnd = getStoredString(UNIVERSE_END_EEPROM_ADDR).toInt();
   String storedDmxAddr = getStoredString(DMX_ADDR_EEPROM_ADDR);
+  String storedReverseArray = getStoredString(B_REVERSE_ARRAY_EEPROM_ADDR);
+  // LED settings
   String storedNrofLEDS = getStoredString(NRLEDS_EEPROM_ADDR);
   String storedB16Bit = getStoredString(B_16BIT_EEPROM_ADDR);  // Add this line
+  // Device settings
   String storedBfailover = getStoredString(B_FAILOVER_EEPROM_ADDR);
   String storedBsilent = getStoredString(B_SILENT_EEPROM_ADDR);
   String storedDeviceName = getStoredString(DEV_NAME_EEPROM_ADDR);
-  int storedUniverseStart = getStoredString(UNIVERSE_START_EEPROM_ADDR).toInt();
-  int storedUniverseEnd = getStoredString(UNIVERSE_END_EEPROM_ADDR).toInt();
-  String storedReverseArray = getStoredString(B_REVERSE_ARRAY_EEPROM_ADDR);
-  
+  // Wifi settings
+  String storedSSID = getStoredString(SSID_EEPROM_ADDR);
+  String storedPassword = getStoredString(PASS_EEPROM_ADDR);
+  // DHCP config 
   String storedDHCP = getStoredString(B_DHCP_EEPROM_ADDR);
   String storedIP = getStoredString(IP_ADDR_EEPROM_ADDR);
   String storedSubnet = getStoredString(IP_SUBNET_EEPROM_ADDR);
@@ -214,7 +169,6 @@ void setup() {
   b_16Bit = storedB16Bit.toInt(); 
   b_failover = storedBfailover.toInt(); // Add this line
   b_silent = storedBsilent.toInt();
-
   b_dhcp = storedDHCP.toInt();
   b_reverseArray = storedReverseArray.toInt();
   IP_ADDR = storedIP;
@@ -222,61 +176,52 @@ void setup() {
   IP_GATEWAY = storedGateway;
   IP_DNS = storedDNS;
   //b_dhcp = storedDHCP;
+// #endregion
 
-
-  if (b_dhcp == 0)
-  {
+  // If DHCP is disabled, try to apply static ip config
+  if (b_dhcp == 0){
     bool bValid = false;
-      Serial.println("Static IP configured..");
-
+      Serial.println("Configure static IP...");
       // Convert IP address from string to IPAddress object
       IPAddress ip;
       IPAddress subnet;
       IPAddress gateway;
       IPAddress dns;
-      if (ip.fromString(IP_ADDR)) {
+      if (ip.fromString(IP_ADDR)) { // check if IP is correct format
           Serial.println("IP Address: "+ IP_ADDR);
-           // Convert subnet from string to IPAddress object
-          
-          if (subnet.fromString(IP_SUBNET)) {
+          if (subnet.fromString(IP_SUBNET)) { // check if subnet mask is correct format
               Serial.println("Subnet Mask: " + IP_SUBNET);
-              bValid = true;
+              if (gateway.fromString(IP_GATEWAY)) { // check if gateway is correct format
+                  Serial.println("Gateway: "+ IP_GATEWAY);
+                  bValid = true; // we have valid config, safe to enable static IP
+              } else {
+                  Serial.println("Invalid Gateway: " + IP_GATEWAY);
+              }               
           } else {
               Serial.println("Invalid Subnet Mask: " + IP_SUBNET);
           }
-
       } else {
           Serial.println("Invalid IP address: " + IP_ADDR);
-      }
-
-     
-      // Convert gateway from string to IPAddress object
-      
-      if (gateway.fromString(IP_GATEWAY)) {
-          Serial.println("Gateway: "+ IP_GATEWAY);
-      } else {
-          Serial.println("Invalid Gateway: " + IP_GATEWAY);
-      }
-
-      // Convert DNS from string to IPAddress object
-
+      }     
+      // Optional DNS address settings
       if (dns.fromString(IP_DNS)) {
           Serial.println("DNS Server: " + IP_DNS);
       } else {
           Serial.println("Invalid DNS Server: " + IP_DNS);
       }
+      // Set static IP on Ethernet if capable and Wifi 
       if (bValid){
-    #ifdef ETH_CAP
+      #ifdef ETH_CAP
         if(!ETH.config(ip, gateway, subnet, dns)){
             Serial.println("Static IP Failed to configure");
         };
-    #endif
+      #endif
         if (!WiFi.config(ip, gateway, subnet, dns)) {
             Serial.println("Static IP Failed to configure");
         }
       }
   }
-
+  
   // Set device name based on the last two digits of the MAC address if not been set by user
   if (storedDeviceName == "" || storedDeviceName == "null") {
     uint8_t mac[6];
@@ -292,8 +237,7 @@ void setup() {
   ArduinoOTA.setHostname(HOST_NAME.c_str());
   WiFi.setHostname(HOST_NAME.c_str());
   delay(100);
- 
-  // Print stored values for debugging
+// #region Print config
   Serial.println("------------------------DEVICE-CONFIG-----------------------------");
   Serial.println("DEV_NAME: " + DEV_NAME);
   Serial.println("SSID: " + storedSSID);
@@ -312,31 +256,25 @@ void setup() {
   Serial.println("Static Gateway: " + IP_GATEWAY);
   Serial.println("Static DNS: " + IP_DNS);
   Serial.println("FallbackWifi: " + String(b_failover > 0 ? "True" : "False"));
-  Serial.println("------------------------------END--------------------------------");
-
-  //Serial.println("DHCP: " + String(storedDHCP));
- 
-
-
   Serial.print("WiFi Hostname: ");
   Serial.println(WiFi.getHostname());
-
-  // Get and print the mDNS name (service name)
   Serial.print("mDNS Name: ");
   Serial.println(MDNS.hostname(0));
-
+  Serial.println("------------------------------END--------------------------------");
+// #endregion 
   // Initialize LED and effect
   effect.initialize();
   led.initialize(NrOfLeds, DmxAddr);
+// #region Networking and webserver setup
   Serial.println("Nework setup start");
   if (led.ethCap) {
-#ifdef ETH_CAP
+  #ifdef ETH_CAP
     Serial.println("Ethernet capable.. checking connection");
     if(ETH.linkUp()) {
       Serial.println("Ethernet connected");
       Serial.println(ETH.localIP());
     }
-#endif
+  #endif
   } else {
     Serial.println("Ethernet not connected starting wifi");
     // Start Access Point if no stored WiFi credentials
@@ -346,8 +284,6 @@ void setup() {
       startAccessPoint(false);
       // Connect to WiFi
       WiFi.begin(storedSSID.c_str(), storedPassword.c_str());
-
-
       // Attempt to connect with a timeout
       int attempts = 0;
       while (WiFi.status() != WL_CONNECTED && attempts < WIFI_ATTEMPT) {
@@ -359,15 +295,14 @@ void setup() {
         Serial.println("Connecting to WiFi...");
         attempts++;
         if (led.ethCap){
-#ifdef ETH_CAP
+  #ifdef ETH_CAP
           if(ETH.linkUp()){
             Serial.println("ETH connected, rebooting...");
             ESP.restart();
           }
-#endif
+  #endif
         }
       }
-
       // If not connected, start Access Point and setup
       if (WiFi.status() != WL_CONNECTED) {
         WiFi.disconnect(false);
@@ -380,7 +315,6 @@ void setup() {
       }
     }
   }
-  
   // if connected start connection guard task to ensure we are alwasy able to connect.
   #ifdef ETH_CAP
   if (b_failover > 0)
@@ -388,19 +322,12 @@ void setup() {
       xTaskCreatePinnedToCore(conGuardTask, "ConGuardTask", 2048, NULL, 3, NULL, 1);
   }
   #endif
-  // If connected, start normal mode
-  Serial.println("Nework setup done");
   MDNS.begin(HOST_NAME.c_str());
   MDNS.disableArduino();
   // add Http service to mdns
   MDNS.addService("http", "tcp", 80);
   // Setup webserver
   setupWebServer();
-  //b_APmode = false;
-
-  // Get and print the mDNS name (service name)
-  Serial.print("mDNS Name: ");
-  Serial.println(MDNS.hostname(0));
   // display led effect when connected
   if (!b_silent > 0)
   {
@@ -408,7 +335,9 @@ void setup() {
   }
   led.blankLEDS(170);
   delay(1);
-
+  Serial.println("Nework setup done");
+// #endregion
+// #region ArtNet init
   // Create FreeRTOS task for ArtNet updates on core 1
   Serial.println("Starting ArtNet");
   //xTaskCreatePinnedToCore(artnetTask, "ArtnetTask", 8192, NULL, 1, NULL, 1);
@@ -444,8 +373,9 @@ void setup() {
       artnet.subscribe(storedUniverseStart + 3, callback3);
       Serial.println("Add universe listener for " + String(storedUniverseStart + 3) + " for callback2");
   }
-  // Begin OTA update
+// #endregion
 
+  // Begin OTA update
   ArduinoOTA.begin();
 
   xTaskCreatePinnedToCore(otaTask, "OtaTask", 8192, NULL, 2, NULL, 1);
